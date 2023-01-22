f7b2b433f7d4fb586dc02c44bfab858efa347753
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2023-01-22 11:27:35 +0530

    22/01/2023

--- JS/Var.js
+++ JS/Var.js
@@ -3,3 +3,14 @@ const b = 12;
 let c = 14;
 console.log(a, b, c)
 console.log(a + b + c)
+var a = 20;
+// var is re declear 
+c = 12;
+// let change value
+//b = 24;
+// const not chance value.
+console.log(typeof a, (a))
+
+
+
+
---
+++ JS/data_types.js
@@ -0,0 +1,5 @@
+const a = {
+    name: "Abhishek",
+    class: 10
+}
+console.log(a.name, a.class)
