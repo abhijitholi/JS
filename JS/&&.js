6803d50dbb858b4a48f3389282cac1d429240dec
abhijitholi <jitholi83@gmail.com>
2023-12-17 12:22:55 +0530

    Create &&.js

---
+++ JS/&&.js
@@ -0,0 +1,16 @@
+const a = 3;
+const b = -2;
+// console.log(a > 2);
+// console.log(b > 0);
+
+// console.log(a > 2 && b > 0);
+console.log(true && true);
+// This line uses the && (logical AND) operator to check if both true conditions are satisfied. Since both operands are true, the result will be true. The output of this line will be: 
+console.log(true && false);
+// Here, the && operator is used to check if both true and false conditions are satisfied. However, since one of the operands is false, the overall result will be false. The output will be:
+console.log(false && true);
+console.log(false && false);
+// always 
+
+
+// Expected output: false
